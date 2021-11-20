04927be70ed15410e011f26bf7c33b3671263005
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-20 13:23:49 +0100

    pdate OrderController

--- src/main/java/com/spring/training/demo/controllers/OrderController.java
+++ src/main/java/com/spring/training/demo/controllers/OrderController.java
@@ -1,28 +1,41 @@
 package com.spring.training.demo.controllers;
 
+import com.spring.training.demo.apiClient.ItemApiClient;
+import com.spring.training.demo.models.Item;
+import com.spring.training.demo.services.ItemService;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.autoconfigure.CircuitBreakerProperties;
+import io.github.resilience4j.common.circuitbreaker.configuration.CircuitBreakerConfigurationProperties;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 public class OrderController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private ItemService itemService;
 
     @GetMapping("/order")
-    @CircuitBreaker(name = "order service", fallbackMethod = "orderFallbackMethod")
     public ResponseEntity<String> createOrder() {
-        String response = restTemplate.getForObject("http://localhost:8081/item", String.class);
-        return ResponseEntity.ok(response);
+        Item response = itemService.getItem();
+        return ResponseEntity.ok(response.toString());
+
     }
 
-    public ResponseEntity<String> orderFallbackMethod() {
-        return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
-                .body("item service is down");
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_GATEWAY)
+                .body(e.getMessage());
     }
 }
